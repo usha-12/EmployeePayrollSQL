08e652bce7d364ca931cdbdd535203a17c62da0a
usha-12 <usha12ahirwar@gmail.com>
2022-11-23 00:50:07 +0530

    retrieve salary data for a particular emp as well as all emp who have joined in a particular data range from the payroll service database

--- CreateDataBaseuc1.sql
+++ CreateDataBaseuc1.sql
@@ -14,4 +14,6 @@ desc Employee_Payroll_service;
 insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Tusar','male','Bidar',40000,'2022-11-21'),('Sanket','male','Hariyana',30000,'2022-11-21'),('Avinash','male','Kolapur',60000,'2022-11-21');
 insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Aditya','male','Patna',60000,'2022-11-21');
 drop table  Employee_Payroll_service;
+SELECT Name,StartDate FROM  Employee_Payroll_service WHERE Name='Aditya';
+SELECT * FROM  Employee_Payroll_service WHERE StartDate BETWEEN CAST('2022-11-21' AS DATE) AND date (now()) ;
 SELECT * FROM Employee_Payroll_service;
