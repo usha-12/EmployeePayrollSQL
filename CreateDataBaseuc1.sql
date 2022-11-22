9028b3a4128836ef9960cbb886dbb1eb73b4d555
usha-12 <usha12ahirwar@gmail.com>
2022-11-23 00:38:13 +0530

    Retrieve all the employee payroll data that is added to payroll service database

---
+++ CreateDataBaseuc1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Employee_payroll_service;
+USE Employee_payroll_service;
+show tables;
+CREATE TABLE Employee_Payroll_service(
+id int auto_increment,
+name varchar(20),
+gender varchar(10),
+city varchar(30),
+Salary float,
+StartDate Date,
+primary key(id)
+);
+desc Employee_Payroll_service;
+insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Tusar','male','Bidar',40000,'2022-11-21'),('Sanket','male','Hariyana',30000,'2022-11-21'),('Avinash','male','Kolapur',60000,'2022-11-21');
+insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Aditya','male','Patna',60000,'2022-11-21');
+drop table  Employee_Payroll_service;
+SELECT * FROM Employee_Payroll_service;
