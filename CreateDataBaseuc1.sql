18c7231fb6d539067dc80fcc8ac31f8f15af1f69
usha-12 <usha12ahirwar@gmail.com>
2022-11-23 11:56:23 +0530

    add gender to employee payroll table and update the rows to reflect the correct employeee gender

--- CreateDataBaseuc1.sql
+++ CreateDataBaseuc1.sql
@@ -14,6 +14,11 @@ desc Employee_Payroll_service;
 insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Tusar','male','Bidar',40000,'2022-11-21'),('Sanket','male','Hariyana',30000,'2022-11-21'),('Avinash','male','Kolapur',60000,'2022-11-21');
 insert into Employee_Payroll_service (name,gender,city,Salary,StartDate) values('Aditya','male','Patna',60000,'2022-11-21');
 drop table  Employee_Payroll_service;
+
 SELECT Name,StartDate FROM  Employee_Payroll_service WHERE Name='Aditya';
 SELECT * FROM  Employee_Payroll_service WHERE StartDate BETWEEN CAST('2022-11-21' AS DATE) AND date (now()) ;
+
+ALTER TABLE  Employee_Payroll_service ADD Gender1 char(1);
+UPDATE  Employee_Payroll_service SET Gender1='M' WHERE Name='Aditya' or name = 'tusar';
+
 SELECT * FROM Employee_Payroll_service;
